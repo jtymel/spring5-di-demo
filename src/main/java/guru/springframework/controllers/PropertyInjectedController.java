435d9f715ba25f7a1b0df0a7fac9d55c44fb32d9
Jan Tymel <jan.tymel@notix.cz>
2018-10-01 10:09:12 +0200

    @Primary

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,15 +1,16 @@
 package guru.springframework.controllers;
 
 import guru.springframework.service.GreetingService;
-import guru.springframework.service.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingService greetingServiceImpl; // bean name
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl; // bean name, overwritten by @Primary though
 
     public String sayHello() {
         return greetingServiceImpl.sayGreeting();
---
+++ src/main/java/guru/springframework/service/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from Primary Greeting Service";
+    }
+}
