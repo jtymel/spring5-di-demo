8d548400e48427ef9d117c9c05ce17e8b5a3ebba
Jan Tymel <jan.tymel@notix.cz>
2018-10-01 09:50:08 +0200

    Spring dependency injection

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,16 +1,20 @@
 package guru.springframework.controllers;
 
 import guru.springframework.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired // optional
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 
